ed9ca0ace6b0b12cd2c452e94bc162ee7a9371df
JC Flores <tothenextcode@gmail.com>
2022-04-13 20:47:03 +0000

    Implemented setupInput and handleUserInput. Ctrl+C will terminate the connection

--- play.js
+++ play.js
@@ -1,4 +1,25 @@
+const { setPriority } = require('os');
 const { connect } = require('./client');
 
-console.log('Connection...');
-connect();
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+}
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    console.log('Connection Terminated!');
+    connection.end();
+    return process.exit();;
+  }
+
+  process.stdout.write(key);
+};
+
+console.log('Connecting...');
+const connection = connect();
+const stdin = setupInput();
